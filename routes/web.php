c227e31be3bb253ca3363140640f59081aaac45b
elyasa9833 <aelyasa09@gmail.com>
2024-01-05 01:42:27 +0700

    feat: role - admin to admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,12 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    
+    // admin
+    Route::get('/admin/dashboard', function(){
+        return view('dashboard');
+    })->name('dashboard');
+
 });
 
 require __DIR__.'/auth.php';
